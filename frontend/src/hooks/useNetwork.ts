8d7bc9acc7e2aa9cadaab84385607fa339e039c7
Chaitanya Sharma <39400946+CheeksTheGeek@users.noreply.github.com>
2022-03-27 05:29:36 -0400

    fixed the mess

--- frontend/src/hooks/useNetwork.ts
+++
@@ -1,9 +0,0 @@
-import { useWallet } from "@terra-money/wallet-provider";
-import networks from "../networks";
-
-const useNetwork = () => {
-  const { network: extNetwork } = useWallet();
-  return networks[extNetwork.name];
-};
-
-export default useNetwork;
